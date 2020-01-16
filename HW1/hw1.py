ae5f7b200bfc605b69d798476b53077fbf3411e2
yakolan213 <57493477+yakolan213@users.noreply.github.com>
2020-01-16 20:04:23 +0200

    Add files via upload

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -4,37 +4,42 @@ import networkx as nx
 import copy
 from itertools import combinations
 
-#num_of_shirts = pd.read_csv("preferences.csv",sep=',', header = None, nrows = 1 )
+### Read file to get data ###
 parse_shirts = pd.read_csv("preferences.csv",nrows=1,skip_blank_lines=1)
 num_of_shirts = int(parse_shirts.keys()[0])
 parse_voters = pd.read_csv("preferences.csv",nrows=1,skip_blank_lines=1,skiprows=(num_of_shirts+1))
 num_of_unique_voters = float(parse_voters.keys()[2])
 num_of_unique_voters = round(num_of_unique_voters)
-#shirts_kind = pd.read_csv("preferences_yakov.csv",nrows=num_of_shirts-1 ,skip_blank_lines=1, skiprows=1)
-shirts = {1:'Australia',2:'Braille',3:'Brush Strokes',4:'Exponential',5:'College',6:'Graph Coloring',
-          7:'Red',8:'Simple',9:'Star Trek',10:'TSP',11:'VRP'}
+# shirts = {1:'Australia',2:'Braille',3:'Brush Strokes',4:'Exponential',5:'College',6:'Graph Coloring',
+#           7:'Red',8:'Simple',9:'Star Trek',10:'TSP',11:'VRP'}
+with open("preferences.csv", 'r') as preferences:
+    shirts_raw = preferences.readlines()[1:num_of_shirts + 1]
+shirts = {}
+for line in shirts_raw:
+    line_objests = line.split(',')
+    shirts[int(line_objests[0])] = line_objests[1]
 col_names = list(range(0, (num_of_shirts+1)))
-#print(col_Names)
 df = pd.read_csv("preferences.csv",skiprows=(num_of_shirts+2),names=col_names)
 num_of_same_pref = df[0]
 our_rule_keys = df.drop([0], axis=1)
 our_rule = []
-### Pairwise Winners ###
+
+### Pairwise Winners - Compute pairwise winners to build a matrix from which we can determine Condorcent Winners. ###
 condorcent = 1
 copeland = shirts.copy()
+#Set Data for Copeland as derive from Condorcent check.
 for key in copeland.keys():
     copeland[key] = 0
 shirts_left = num_of_shirts
 c_winners = list()
 votes = shirts.copy()
+#Look for all Condorcent winners
 while(condorcent == 1):
     pairwise_matrix = np.zeros( (num_of_shirts, num_of_shirts) )
     tmp_df = copy.deepcopy(df)
     tmp_df = tmp_df.drop([0], axis=1)
     len_competitors = len(votes.keys())
     competitors = list(votes.keys())
-    #len_competitors = len(tmp_df.keys())
-    #competitors = list(tmp_df.keys())
     shirt1,shirt2 = 0,0
     for x in range(len_competitors):
         for y in range(len_competitors):
@@ -88,6 +93,7 @@ while(condorcent == 1):
             break
         else:
             condorcent = 0
+
 ### Plurality ###
 for key in votes.keys():
     votes[key] = 0
@@ -108,7 +114,6 @@ for key in votes.keys():
     votes[key] = 0
 temp_df = copy.deepcopy(df)
 temp_df = temp_df.drop([0], axis=1)
-#votes = dict([(key, 0) for key in temp_df.keys()])
 for row in range(0, num_of_unique_voters, 1):
     shirts_counting = shirts_left-1
     array = temp_df.loc[[row]]
@@ -123,12 +128,9 @@ print("Borda winner:",shirts[winner],",scores:",votes)
 ### Nanson ###
 temp_df = copy.deepcopy(df)
 temp_df = temp_df.drop([0], axis=1)
-#votes = dict([(key, 0) for key in temp_df.keys()])
 winners = list(votes.keys())
 winner,same_mean=0,0
 remove_list = list()
-#for i in range(len_c):
-    #winners.remove(c_winners[i])
 round = 1
 scores = copy.deepcopy(votes)
 for key in scores.keys():
@@ -137,7 +139,6 @@ while(len(winners)>1 and winner ==0):
     round += 1
     for key in votes.keys():
         votes[key] = 0
-    #votes = dict([(key, 0) for key in temp_df.keys()])
     sum = 0
     for row in range(0, num_of_unique_voters, 1):
         shirts_left = len(winners) - 1
@@ -176,7 +177,7 @@ for key in scores.keys():
     scores[key] = 1
 tmp_df = copy.deepcopy(df)
 tmp_df = tmp_df.drop([0], axis=1)
-while(winner==0):
+while(len(votes)>1):
     for key in votes.keys():
         votes[key] = 0
         if(round>1):
@@ -186,27 +187,51 @@ while(winner==0):
         for element in array:
             if (int(array[element]) != 0):
                 same_pref = num_of_same_pref[row]
-                votes[int(array[element])] += same_pref
+                votes[int(array[element])] += int(same_pref)
                 break
-    leader = max(votes, key=votes.get)
-    remove_list = list()
+    eliminate = min(votes, key=votes.get)
     for key in votes.keys():
-        if(votes[key]==votes[leader] and key!=leader):
-            remove_list.append(min(votes, key=votes.get))
-    if(len(remove_list)==0):
-            winner = leader
-    for i in remove_list:
-        votes.pop(i)
-        tmp_df[tmp_df[:] == i + 1] = 0
-    remove_list = list()
+        if(votes[key]==votes[eliminate] and shirts[key]>shirts[eliminate]):
+            eliminate = key
+    votes.pop(eliminate)
+    tmp_df[tmp_df[:] == eliminate] = 0
     round += 1
-print("STV winner:", shirts[winner],"scores:",scores)
+print("STV winner:", shirts[list(votes.keys())[0]],"scores:",scores)
 
 ### Copeland ###
 winner = max(copeland, key=copeland.get)
 for i in c_winners:
     copeland.pop(i, None)
 print("Copeland winner:",shirts[winner],",scores:",copeland)
+
+### PageRank ###
+tran_matrix = np.transpose(pairwise_matrix)
+G = nx.DiGraph(tran_matrix)
+SG=nx.stochastic_graph(G)
+iterations = 100
+alpha = 0.85
+x=dict.fromkeys(SG, 1.0 / SG.number_of_nodes())
+num_of_nodes=SG.number_of_nodes()
+out_degree=SG.out_degree(weight=None)
+for i in range(iterations):
+    x_last=x
+    x=dict.fromkeys(x_last.keys(), 0)
+    sum_values = 0
+    for value in x_last.values():
+        sum_values += value
+    teleport_sum= (1.0 - alpha) / num_of_nodes * sum_values
+    for n in x:
+        for num in SG[n]:
+            x[num]+= alpha * x_last[n]*SG[n][num]['weight']
+        x[n] += teleport_sum
+    inv=1.0/sum_values
+    for n in x:
+        x[n]*=inv
+        x[n] = float("{0:.5f}".format(x[n]))
+pagerank = votes
+for key in x.keys():
+    pagerank[int(key+1)] = x[key]
+print("PageRank:",pagerank)
 
 ### Kendal-Tau Distance ###
 profile_num1 = num_of_unique_voters - 1
@@ -223,77 +248,16 @@ for x in profile1:
 for y in profile2:
     if(y!=0):
         rank_b.append(y)
+pairs = combinations(range(1, len(rank_a)+1), 2)
 dist = 0
-n_candidates = len(rank_a)
-for i, j in combinations(range(n_candidates), 2):
-    dist += (np.sign(rank_a[i] - rank_a[j]) ==
-            -np.sign(rank_b[i] - rank_b[j]))
+for x, y in pairs:
+    a = rank_a.index(x) - rank_a.index(y)
+    b = rank_b.index(x) - rank_b.index(y)
+    if a * b < 0:
+        dist += 1
 print("Profile 1:",rank_a)
 print("Profile 2:",rank_b)
 print("Kendal-Tau Distance:",dist)
-
-# print(pairwise_matrix)
-# print(np.transpose(pairwise_matrix))
-# G = nx.DiGraph(np.transpose(pairwise_matrix))
-# print(nx.pagerank(G))
-
-### PageRank ###
-def pagerank(G, alpha=0.85, personalization=None,
-             max_iter=100, tol=1.0e-6, nstart=None, weight='weight',
-             dangling=None):
-    if not G.is_directed():
-        D = G.to_directed()
-    else:
-        D = G
-
-        # Create a copy in (right) stochastic form
-    W = nx.stochastic_graph(D, weight=weight)
-    N = W.number_of_nodes()
-
-    # Choose fixed starting vector if not given
-    if nstart is None:
-        x = dict.fromkeys(W, 1.0 / N)
-    else:
-        # Normalized nstart vector
-        s = float(sum(nstart.values()))
-        x = dict((k, v / s) for k, v in nstart.items())
-
-    if personalization is None:
-
-        # Assign uniform personalization vector if not given
-        p = dict.fromkeys(W, 1.0 / N)
-    else:
-        missing = set(G) - set(personalization)
-        s = float(sum(personalization.values()))
-        p = dict((k, v / s) for k, v in personalization.items())
-
-    if dangling is None:
-
-        # Use personalization vector if dangling vector not specified
-        dangling_weights = p
-    else:
-        missing = set(G) - set(dangling)
-        s = float(sum(dangling.values()))
-        dangling_weights = dict((k, v / s) for k, v in dangling.items())
-    dangling_nodes = [n for n in W if W.out_degree(n, weight=weight) == 0.0]
-
-    # power iteration: make up to max_iter iterations
-    for _ in range(max_iter):
-        xlast = x
-        x = dict.fromkeys(xlast.keys(), 0)
-        danglesum = alpha * sum(xlast[n] for n in dangling_nodes)
-        for n in x:
-
-            # this matrix multiply looks odd because it is
-            # doing a left multiply x^T=xlast^T*W
-            for nbr in W[n]:
-                x[nbr] += alpha * xlast[n] * W[n][nbr][weight]
-            x[n] += danglesum * dangling_weights[n] + (1.0 - alpha) * p[n]
-
-            # check convergence, l1 norm
-        err = sum([abs(x[n] - xlast[n]) for n in x])
-        if err < N * tol:
-            return x
 
 ### Our Rule - Tournamnet/Playoffs (more info in PDF file) ###
 A = list(our_rule_keys.keys())
